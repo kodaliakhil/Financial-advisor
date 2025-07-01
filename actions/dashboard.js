df402591300f757c42a30fdfc721dc68a43a4164
kodaliakhil <kodaliakhil1999@gmail.com>
2025-07-01 18:59:14 +0530

    getUserAccounts functionality, Displaying all user account card in Dashboard

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -13,6 +13,9 @@ const serializeTransaction = (obj) => {
   if (obj.balance) {
     serialized.balance = obj.balance.toNumber();
   }
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
   return serialized;
 };
 
@@ -60,6 +63,36 @@ export async function createAccount(data) {
 
     revalidatePath("/dashboard");
     return { success: true, data: serializedAccount };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getUserAccounts() {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const accounts = await db.account.findMany({
+      where: { userId: user.id },
+      orderBy: { createdAt: "desc" },
+      include: {
+        _count: {
+          select: {
+            transactions: true,
+          },
+        },
+      },
+    });
+    const serializedAccount = accounts.map(serializeTransaction);
+    return serializedAccount;
   } catch (error) {
     throw new Error(error.message);
   }
