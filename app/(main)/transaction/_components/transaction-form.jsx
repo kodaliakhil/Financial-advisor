252de6f3f41be36d65aafd35fb5182f958d492ed
kodaliakhil <kodaliakhil1999@gmail.com>
2025-07-07 21:32:13 +0530

    transaction form API, Form schema

---
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+import React from 'react'
+
+const AddTransactionForm = () => {
+  return (
+    <div>AddTransactionForm</div>
+  )
+}
+
+export default AddTransactionForm
--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import React from "react";
+import AddTransactionForm from "../_components/transaction-form";
 
-const TransactionCreatePage = () => {
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
   return (
-    <div>TransactionCreatePage</div>
-  )
-}
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient gradient-title mb-8 ">
+        Add Transaction
+      </h1>
+      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+    </div>
+  );
+};
 
-export default TransactionCreatePage
+export default AddTransactionPage;
